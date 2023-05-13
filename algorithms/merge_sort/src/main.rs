5a64e34c357471b2417b44c7692c593f10846904
Boukari Yameogo <yameogo.boukari@gmail.com>
2023-05-13 01:36:59 -0400

    merge sort implemented with test (#2)

--- algorithms/merge_sort/src/main.rs
+++ algorithms/merge_sort/src/main.rs
@@ -1,26 +1,29 @@
-use bubble_sort::bubble_sort;
-use std::iter::repeat_with;
+use merge_sort::merge_sort;
 use rand::{Rng, thread_rng};
-use std::env;
+use std::env::args;
+use std::iter::repeat_with;
 use std::process;
 
 
 fn main() {
-    let args: Vec<String> = env::args().skip(1).collect();
+   let args: Vec<String> = args().skip(1).collect();
     if args.len() != 1 {
-        eprintln!("Please enter a valid number");
+        eprintln!("Usage: merge_sort <number of elements>");
         process::exit(1);
     }
     let n: i64 = match args[0].parse() {
-        Ok(v) => v,
-        Err(e) => panic!("Invalid number!!!. Error {}", e),
+        Ok(n) => n,
+        Err(_) => {
+            eprintln!("Usage: merge_sort <number of elements>");
+            process::exit(1);
+        }
     };
-
     let mut rng = thread_rng();
     let mut my_vec: Vec<i64> = repeat_with(|| rng.gen_range(0..n))
         .take(n as usize)
         .collect();
     println!("Initial vector: {:?}", my_vec);
-    bubble_sort(&mut my_vec);
+    my_vec = merge_sort(my_vec);
     println!("Sorted vector: {:?}", my_vec);
+
 }
