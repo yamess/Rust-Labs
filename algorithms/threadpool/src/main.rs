d353af8f66d3319c4683fdfb58f165d60707a593
Boukari Yameogo <yameogo.boukari@gmail.com>
2023-05-22 00:12:30 -0400

    fix

--- algorithms/threadpool/src/main.rs
+++ algorithms/threadpool/src/main.rs
@@ -5,8 +5,8 @@ mod threadpool;
 use threadpool::ThreadPool;
 
 fn main() {
-    let pool = ThreadPool::new(20);
-    for i in 0..100 {
+    let pool = ThreadPool::new(10);
+    for i in 0..10000 {
         pool.execute(move || {
             println!("Task {} is being executed", i);
         });
